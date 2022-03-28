bdbe3f261c58edc7d56d9d494f2c00c118b40ede
Donatella7 <campaniellodonatella@gmail.com>
2022-03-28 15:33:30 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,12 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  padding: 0 8rem;
+  box-sizing: border-box;
+}
+
+.d-flex{
+  display: flex;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-<div></div>
+    <div class="my-header-content d-flex">
+        <div class="my-header-logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="my-header-nav">
+            <ul class="d-flex my-link-list" >
+                <li class="my-link-item" v-for="(link,index) in headerLinks" :key="index" >{{link.text}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,9 +16,47 @@ export default {
 
     name: "HeaderDc",
 
+    data(){
+        return{
+            headerLinks:[
+
+                {text:"Characters", url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"},
+                {text:"Comics",  url:"#"}
+            ]
+        }
+
+    }
+
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.my-header-content{
+    justify-content: space-between;
+    padding: 1rem;
+    background-color:red;//solo per vederlo
+}
+
+.my-header-nav{
+
+    line-height: 106.5px;
+    .my-link-list{
+        list-style-type:none;
+        margin: 0;
+
+        .my-link-item{
+            padding: .5rem;
+        }
+    }
+}
+
+
 
 </style>
